6121e056c3898cf3ddc4cf8abb79a8516d7d1a65
Andreas Jonsson <andreas.jonsson@nav.no>
2025-09-14 20:47:22 +0200

    WIP

--- src/main/kotlin/no/nav/klage/service/KabalApiService.kt
+++ src/main/kotlin/no/nav/klage/service/KabalApiService.kt
@@ -16,21 +16,21 @@ object KabalApiService {
     suspend fun fetchAndStoreBehandlinger() {
         val client = HttpClient(CIO)
 
-        val response = client.get("https://kabal-api/api/kaptein/behandlinger-stream") {
-            // Add necessary headers, authentication, etc.
-            header("Accept", "application/x-ndjson")
-
-        }
-
-        // Check if the response is successful and then stream the body
-        if (response.status.isSuccess()) {
-            val channel = response.bodyAsChannel()
-            while (!channel.isClosedForRead) {
-                val packet = channel.readRemaining(1024) // Read in chunks
-                // TODO: Process
-                logger.debug("Received chunk: ${packet.readText()}")
-            }
-        }
+//        val response = client.get("https://kabal-api/api/kaptein/behandlinger-stream") {
+//            // Add necessary headers, authentication, etc.
+//            header("Accept", "application/x-ndjson")
+//
+//        }
+//
+//        // Check if the response is successful and then stream the body
+//        if (response.status.isSuccess()) {
+//            val channel = response.bodyAsChannel()
+//            while (!channel.isClosedForRead) {
+//                val packet = channel.readRemaining(1024) // Read in chunks
+//                // TODO: Process
+//                logger.debug("Received chunk: ${packet.readText()}")
+//            }
+//        }
 
         BehandlingRepository.clearAndAddAll(emptyList())
     }
